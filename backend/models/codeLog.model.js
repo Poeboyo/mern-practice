27c07d8f9f7a697726170664a20a118a36c96e9f
Sam Nee <samuel.r.nee@gmail.com>
2020-04-06 18:29:04 -0400

    set up schemas

---
+++ backend/models/codeLog.model.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const codeLogSchema = new Schema({
+    username: { type: String, required: true },
+    description: { type: String, required: true },
+    duration: { type: Number, required: true },
+    date: { type: Date, required: true },    
+}, {
+    timestamps: true,
+});
+
+const CodeLog = mongoose.model('CodeLog', codeLogSchema);
+
+module.exports = CodeLog
